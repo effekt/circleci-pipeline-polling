a443119c0c5db94506a6060f18440b8c37204c5c
Jesse Wheeler <jesse@i-leet.com>
2022-05-17 18:14:02 -0400

    feat: initial action creation

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # circleci-pipeline-polling
-CircleCI Pipeline Polling GitHub Action
+
+A GitHub action to wait for all CircleCI checks on a pipeline to succeed.
+
+### Steps:
+
+- Have CircleCI post event to GitHub with Pipeline ID to your branch
+  - https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows#workflow_dispatch
+- Add a new workflow under `.github/workflows`
+
+`.github/workflows/circleci-pipeline-polling`
+
+README TBD
---
+++ index.js
@@ -0,0 +1,87 @@
+const fetch = require('node-fetch');
+const core = require('@actions/core');
+
+const { Headers, Request } = fetch;
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const getPipelineWorkflows = async (pipelineId, circleCiToken, pageToken) => {
+  const headers = new Headers({
+    'Circle-Token': circleCiToken
+  });
+
+  const request = new Request(
+    `https://circleci.com/api/v2/pipeline/${pipelineId}/workflow${pageToken && `?page-token=${pageToken}`}`,
+    {
+      method: 'GET',
+      headers: headers,
+    }
+  );
+
+  const response = await fetch(request);
+  const json = await response.json();
+
+  let items = json.items;
+  let next_page_token = json.next_page_token;
+
+  if (next_page_token) {
+    return [...items, ...(await getPipelineWorkflows(pipelineId, circleCiToken, next_page_token))];
+  }
+
+  return items;
+}
+
+(async () => {
+  try {
+    const inputCciToken = core.getInput('token', { required: true });
+    const inputPipelineId = core.getInput('pipeline', { required: true });
+    const inputPollRate = core.getInput('interval');
+    const inputTimeout = core.getInput('timeout');
+
+    const pollRate = Number.parseInt(inputPollRate || 10) * 1000;
+    const timeout = Number.parseInt(inputTimeout || 15) * 60000;
+
+    const maxRunTime = new Date(new Date().getTime() + timeout).getTime();
+
+    const failingStatuses = ['cancelled', 'error', 'failed', 'failing', 'unauthorized'];
+    const successfulStatuses = ['on_hold', 'success'];
+
+    while(true && !(new Date().getTime() > maxRunTime)) {
+      const workflows = await getPipelineWorkflows(inputPipelineId, inputCciToken).map(workflow => {
+        const { name, status } = workflow;
+
+        return { name, status };
+      });
+
+      console.log(`Workflows present on CircleCI Pipeline ID (${inputPipelineId}):`);
+      console.table(workflows);
+      console.log('\n');
+
+      const failedWorkflow = workflows.find((workflow) => {
+        failingStatuses.includes(workflow.status);
+      });
+
+      if (failedWorkflow) {
+        core.setFailed(`Failed on workflow: ${failedWorkflow.name}`);
+        return;
+      }
+
+      const successfulWorkflows = workflows.filter(workflow => {
+        successfulStatuses.includes(workflow.status);
+      });
+
+      if (successfulWorkflows.length === workflows.length) {
+        console.log(`All workflows passed for pipeline ${pipelineId}!`);
+        
+        return;
+      }
+      
+      console.log(`Waiting ${pollRate}s`);
+      await delay(pollRate);
+    }
+
+    return;
+  } catch(err) {
+    core.setFailed(`Error: ${err.message}`)
+  }
+})();
